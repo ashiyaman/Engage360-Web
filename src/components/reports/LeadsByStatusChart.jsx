66adaed96241f60a29b99ec3257219404775e1b0
ashiyaman <ashiya1005@gmail.com>
2026-07-02 19:00:00 +0530

    Fix: Fix leadsbystatus chart

--- src/components/reports/LeadsByStatusChart.jsx
+++ src/components/reports/LeadsByStatusChart.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import getAnalytics from "../../utils/analytics";
 import { LeadContext } from "../../contexts/LeadContext";
-import { ResponsiveContainer, PieChart, Pie } from "recharts";
+import { ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from "recharts";
 
 const LeadsByStatusChart = () => {
     const {leads} = useContext(LeadContext)
@@ -12,11 +12,19 @@ const LeadsByStatusChart = () => {
     return (
         <div style={{ width: '100%', height: 300 }}>
           <h2>Leads By status</h2>
-          <ResponsiveContainer>
-            <PieChart>
-              <Pie dataKey="value" data={data} fill="#8884d8" label={({ name, value }) => `${name}: ${value}`} />
-            </PieChart>
-          </ResponsiveContainer>
+           <ResponsiveContainer width="100%" height="100%" >
+        <BarChart data={data} layout="vertical" margin={{ top: 20, right: 30, left: 40, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+
+          <XAxis type="number" />
+          <YAxis dataKey="name" type="category" />
+
+          <Tooltip />
+          <Legend />
+
+          <Bar dataKey="value" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
         </div>
       );
 }
